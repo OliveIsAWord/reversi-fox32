f0777424621193eeebdbb99e3e764354726be49b
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2025-03-03 23:24:55 -0700

    manually rendering to screen

--- gen_bindings.rs
+++ gen_bindings.rs
@@ -4,6 +4,12 @@ let args: Vec<_> = std::env::args().collect();
 assert_eq!(args.len(), 2);
 let lines: Vec<_> = std::fs::read_to_string(&args[1])?.lines().map(str::to_owned).collect();
 for (i, address) in lines.iter().enumerate().filter_map(|(i, line)| line.strip_prefix("##").map(|l|(i, l.trim()))) {
-    let name = lines[i + 1].strip_prefix("extern fn ").unwrap().split("(").next().unwrap().to_ascii_uppercase();
+    let name = lines[i + 1]
+        .strip_prefix("extern fn ")
+        .unwrap()
+        .split("(")
+        .next()
+        .unwrap()
+        .to_ascii_uppercase();
     println!("const {name}: {address}");
 }
